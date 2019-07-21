cde774affa97b42393f69a7f93d548aba5966958
howmanysmall <lhamil29@bruinmail.slcc.edu>
2019-07-21 17:02:22 -0600

    Update Util.lua

--- lib/Util.lua
+++ lib/Util.lua
@@ -21,8 +21,6 @@ local zeroVector = vec3(0, 0, 0)
 local uz = vec3(0, 0, 1)
 local ux = vec3(1, 0, 0)
 local sqrt = math.sqrt
-local dot = zeroVector.Dot
-local cross = zeroVector.Cross
 
 --[[
 -- Unused because too expensive to be worth the benefit since you'd have to call it often
@@ -41,26 +39,26 @@ end
 
 -- Making a CFrame value from an origin position and unit direction:
 function Util.makeOrientation(sourcePos, dir)
-    -- Construct CFrame to rotate parts from x-axis alignment (where they are constructed) to
-    -- the world orientation of the arc from a quaternion rotation that describes the
-    -- rotation of ux-->axis
-    dir = dir.unit
+	-- Construct CFrame to rotate parts from x-axis alignment (where they are constructed) to
+	-- the world orientation of the arc from a quaternion rotation that describes the
+	-- rotation of ux-->axis
+	dir = dir.Unit
 
-    -- Proportionate angle between ux and direction:
-    local angleUxAxis = dot(ux, dir)
+	-- Proportionate angle between ux and direction:
+	local angleUxAxis = ux:Dot(dir)
 
-    if (angleUxAxis > 0.99999) then
-        return cframe(sourcePos, sourcePos - uz)
-    elseif (angleUxAxis < -0.99999) then
-        return cframe(sourcePos, sourcePos + uz)
-    else
-        local q = cross(ux, dir)
-        local qw = 1 + angleUxAxis
-        local qnorm = sqrt(q.magnitude ^ 2 + qw * qw)
-        q = q / qnorm
-        qw = qw / qnorm
-        return cframe(sourcePos.x, sourcePos.y, sourcePos.z, q.x, q.y, q.z, qw)
-    end
+	if (angleUxAxis > 0.99999) then
+		return cframe(sourcePos, sourcePos - uz)
+	elseif (angleUxAxis < -0.99999) then
+		return cframe(sourcePos, sourcePos + uz)
+	else
+		local q = ux:Cross(dir)
+		local qw = 1 + angleUxAxis
+		local qnorm = sqrt(q.Magnitude * q.Magnitude + qw * qw)
+		q = q / qnorm
+		qw = qw / qnorm
+		return cframe(sourcePos.X, sourcePos.Y, sourcePos.Z, q.X, q.Y, q.Z, qw)
+	end
 end
 
 return Util
